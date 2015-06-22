405648eff9d681de469c224901eb3be9ec56418a
Thomas Markiewicz <thomas.markiewicz@gmail.com>
2015-06-22 15:38:46 -0500

    Small updates that will hopefully fix the issue.

--- src/ios/NativeStorage.h
+++ src/ios/NativeStorage.h
@@ -1,16 +1,4 @@
-// /////////////////////////////////////////////////////////////////////////////////////
-//                           Copyright (c) 2013 - 2013
-//                            Coyote Logistics L.L.C.
-//                          All Rights Reserved Worldwide
-//
-// WARNING:  This program (or document) is unpublished, proprietary
-// property of Coyote Logistics L.L.C. and is to be maintained in strict confidence.
-// Unauthorized reproduction, distribution or disclosure of this program
-// (or document), or any program (or document) derived from it is
-// prohibited by State and Federal law, and by local law outside of the U.S.
-// /////////////////////////////////////////////////////////////////////////////////////
-
-#import <Cordova/CDV.h>
+#import <Cordova/CDVPlugin.h>
 
 @interface NativeStorage : CDVPlugin
 
--- src/ios/NativeStorage.m
+++ src/ios/NativeStorage.m
@@ -1,16 +1,5 @@
-// /////////////////////////////////////////////////////////////////////////////////////
-//                           Copyright (c) 2013 - 2013
-//                            Coyote Logistics L.L.C.
-//                          All Rights Reserved Worldwide
-//
-// WARNING:  This program (or document) is unpublished, proprietary
-// property of Coyote Logistics L.L.C. and is to be maintained in strict confidence.
-// Unauthorized reproduction, distribution or disclosure of this program
-// (or document), or any program (or document) derived from it is
-// prohibited by State and Federal law, and by local law outside of the U.S.
-// /////////////////////////////////////////////////////////////////////////////////////
-
 #import "NativeStorage.h"
+#import <Cordova/CDV.h>
 
 @implementation NativeStorage
 
@@ -30,6 +19,5 @@
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:value];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
-
 
 @end
