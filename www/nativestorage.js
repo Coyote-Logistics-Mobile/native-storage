007f4fce986d9ac262c9b91a5ad19b3ce7629934
Thomas Markiewicz <thomas.markiewicz@gmail.com>
2015-06-23 07:26:50 -0500

    Exposing in window object now as well.

--- www/nativestorage.js
+++ www/nativestorage.js
@@ -9,4 +9,5 @@ NativeStorage.prototype.getValue = function(key, successCallback, failureCallbac
 
 var nativeStorage = new NativeStorage();
 module.exports = nativeStorage;
+window.nativestorage = nativeStorage;
 
